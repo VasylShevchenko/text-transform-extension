b4fbb7b706b615731058e94e78a7d53c85142e54
Vasyl Shevchenko <vasya.shevchenko.2011@gmail.com>
2020-02-04 19:09:38 +0200

    module add Pipe: LowerCase, UpperCase, TitleCase

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LowerCasePipe, UpperCasePipe, TitleCasePipe } from '@angular/common';
+import { AlertService } from './_alert/alert.service';
 
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ],
+  providers: [
+    LowerCasePipe,
+    UpperCasePipe,
+    TitleCasePipe,
+    AlertService
+  ]
 })
 export class AppRoutingModule { }
